d2477ff32bc77b7f13b4f8792e6ba203624ba446
ryoogata <ryo.ogata@gmail.com>
2014-06-23 18:22:42 +0900

    基本設定を default で実施
    db 周りは別 recipe で実行

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,33 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-mysql_database "#{node['eccube']['database']['_DATABASE_NAME']}" do 
-  connection( :host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password'] )
-  action :create
+%w{
+  php-mbstring unzip
+}.each do |package_name|
+    package package_name do
+    action :install
+  end
+end
+
+cookbook_file "/tmp/eccube-2.13.2.zip" do
+  source "eccube-2.13.2.zip"
+  not_if {File.exists?("/tmp/eccube-2.13.2.zip")}
+end
+
+script "copy_files" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    unzip eccube-2.13.2.zip 
+    cp -a eccube-2.13.2/data eccube-2.13.2/html
+    cp -a eccube-2.13.2/html /var/www/
+  EOH
+end
+
+template "/var/www/html/define.php" do
+  source "define.php.erb"
+  owner "root"
+  group "root"
+  mode 0666
 end
