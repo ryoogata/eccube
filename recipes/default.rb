b320fd8586616dfd6d8df810b137157f432f0f52
ryoogata <ryo.ogata@gmail.com>
2014-06-23 14:36:53 +0900

    db 作成

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+mysql_database 'oracle_rules' do 
+  connection( :host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password'] )
+  action :create
+end
